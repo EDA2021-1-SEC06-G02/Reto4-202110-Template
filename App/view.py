3758fb22a0d82df62eee46cd0479d61675462ac4
poloivaz <m.zuliani@uniandes.edu.co>
2021-05-19 23:07:54 -0500

    Primera entrega – Reto 4

--- App/view.py
+++ App/view.py
@@ -36,7 +36,7 @@ sys.setrecursionlimit(2 ** 20)
 La vista se encarga de la interacción con el usuario
 Presenta el menu de opciones y por cada seleccion
 se hace la solicitud al controlador para ejecutar la
-operación solicitada
+operación solicitada.
 """
 
 def printMenu():
